37467d8181731d2442d95ed68ecc3414b1904b19
David Ortiz <david.ortiz@deployedservices.com>
2023-10-22 14:24:35 -0400

    font is working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,23 @@
 import localFont from 'next/font/local';
 
-const mezcal = localFont({ src: '../../public/fonts/Mezcal' });
+const mezcal = localFont({ src: '../../fonts/mezcal.otf' });
 export function Hero() {
 	return (
 		<section className='bg-slate-800'>
 			<div className='mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center'>
 				<div className='mx-auto max-w-xl text-center'>
-					<h1 className='text-9xl font-extrabold sm:text-9xl'>CARLA'S</h1>
-					<h2 className='text-4xl'>comunidad</h2>
+					<h1
+						style={mezcal.style}
+						className='text-9xl font-extrabold sm:text-9xl'
+					>
+						CARLA'S
+					</h1>
+					<h2
+						style={mezcal.style}
+						className='text-4xl'
+					>
+						comunidad
+					</h2>
 
 					<p className='mt-4 sm:text-xl/relaxed'>
 						Using 15+ years of classNameroom experience to bring students of all
